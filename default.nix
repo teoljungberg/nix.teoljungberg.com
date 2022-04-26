a8dea2d5a87b8ccf00fc3353c6006f0842d0b787
Teo Ljungberg <teo@teoljungberg.com>
2022-04-26 14:35:21 +0000

    Leverage the same default.nix file

--- default.nix
+++ default.nix
@@ -1,9 +1,15 @@
+{ localCheckout ? false }:
+
 let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") { };
   lib = pkgs.lib;
   stdenv = pkgs.stdenv;
 
-  dotfiles = fetchTarball "https://github.com/teoljungberg/dotfiles/archive/master.tar.gz";
+  dotfiles =
+    if localCheckout then
+      import ./nix/dotfiles.nix { inherit pkgs; }
+    else
+      fetchTarball "https://github.com/teoljungberg/dotfiles/archive/master.tar.gz";
   teoljungberg = import ./nix/teoljungberg { inherit pkgs dotfiles; };
 
   zsh = teoljungberg.zsh;
--- test/default.nix
+++ test/default.nix
@@ -1,14 +1,1 @@
-let
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") { };
-  lib = pkgs.lib;
-  stdenv = pkgs.stdenv;
-
-  dotfiles = import ../nix/dotfiles.nix { inherit pkgs; };
-  teoljungberg = import ../nix/teoljungberg { inherit pkgs dotfiles; };
-
-  zsh = teoljungberg.zsh;
-in
-if lib.inNixShell then
-  pkgs.mkShell { shellHook = "${zsh}${zsh.shellPath}; exit $?"; }
-else
-  zsh
+import ../default.nix { localCheckout = true; }
