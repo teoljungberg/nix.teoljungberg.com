0a5152eabdda424b870169bcb0d61e41220761c8
Teo Ljungberg <teo@teoljungberg.com>
2022-04-27 09:51:51 +0200

    Set pkgs in function

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{ localCheckout ? false }:
+{ pkgs ? (
+    import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") { }
+  )
+, localCheckout ? false
+}:
 
 let
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") { };
   lib = pkgs.lib;
 
   dotfiles = import ./nix/dotfiles.nix { inherit pkgs localCheckout; };
