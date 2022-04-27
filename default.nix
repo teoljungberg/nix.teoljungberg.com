5ccc82a4dc6d141ca97e67109c396ae10a1e5886
Teo Ljungberg <teo@teoljungberg.com>
2022-04-27 09:28:13 +0200

    Remove unused stdenv

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@
 let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") { };
   lib = pkgs.lib;
-  stdenv = pkgs.stdenv;
 
   dotfiles =
     if localCheckout then
