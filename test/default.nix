854bff49cad89a7ce92911707a238268c7ee523d
Teo Ljungberg <teo@teoljungberg.com>
2022-04-28 17:51:45 +0200

    wip

--- test/default.nix
+++ test/default.nix
@@ -1,1 +1,29 @@
-import ../default.nix { localCheckout = true; }
+let
+  pkgs = (
+    import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") { }
+  );
+  project = import ../default.nix { localCheckout = true; };
+  stdenv = pkgs.stdenv;
+in
+stdenv.mkDerivation {
+  name = "test-nix-on-rails";
+  buildInputs = [
+    project
+    ./version-test.sh
+  ];
+  src = ./.;
+  phases = pkgs.lib.optional stdenv.isLinux [ "unpackPhase" ] ++ [ "noPhase" ];
+  noPhase = ''
+    mkdir $out
+    ln -s ${project} $out/project
+    ln -s ${./version-test.sh} $out/version-test.sh
+    cat <<EOS>> $out/test.sh
+#!/bin/sh
+
+sh $out/version-test.sh "$out/nix-on-rails"
+EOS
+    chmod +x "$out/test.sh"
+
+    sh "$out/test.sh"
+  '';
+}
