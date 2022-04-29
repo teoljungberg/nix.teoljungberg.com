3e1a66d02c48fe64e5f4762e6927a01385e93c6b
Teo Ljungberg <teo@teoljungberg.com>
2022-04-29 10:35:06 +0200

    Extract overlays let

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,9 +1,10 @@
 { dotfiles ? import ./dotfiles.nix { } }:
 
 let
+  overlays = import (dotfiles.getFile "config/nixpkgs/overlays.nix");
   pkgs = (
     import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz") {
-      overlays = import (dotfiles.getFile "config/nixpkgs/overlays.nix");
+      inherit overlays;
     }
   );
 in
